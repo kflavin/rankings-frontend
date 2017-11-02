092007709d2c33abfee31fd1afac587b051a059e
Kyle <Kyle.Flavin@gmail.com>
2017-11-01 23:39:09 -0700

    adding lookahead capabilities to submission page

--- components/SubmitItem.vue
+++ components/SubmitItem.vue
@@ -1,20 +1,109 @@
 <template>
-  <div>{{rank}}: <input type="text" @keyup="hello"></div>
+  <div>
+    <div>{{rank}}: <input type="text" v-model="query" @blur.prevent="displayList=false"></div>
+    <div>
+
+      <div v-show="displayList">
+        <transition-group name="fade" tag="ul" class="Results">
+          <li v-for="item in filtered" :key="item" @mousedown="choose(item)">
+            <span>{{item}}</span>
+          </li>
+        </transition-group>
+      </div>
+
+
+    </div>
+
+  </div>
 </template>
 
 <script>
   export default {
     props: {
-      team: "",
-      rank: 0
+      team: '',
+      rank: {
+        type: Number,
+        required: true
+      },
+      startAt: {
+        type: Number,
+        default: 2
+      },
+      selections: {
+        type: Array,
+        default: []
+      }
     },
-    created: function() {
-      console.log(this.$store.state.teams)
+    computed: {
+      filtered() {
+        console.log("filtering")
+        console.log(this.query)
+        console.log(this.selections)
+        if (this.query.length >= this.startAt) {
+          var items = this.items.filter(item => {
+            return item.toLowerCase().indexOf(this.query.toLowerCase()) > -1 && this.query.length < item.length && !this.selections.includes(item)
+          })
+
+          if (items.length >= 1) {
+            this.displayList = true
+          }
+
+          return items
+
+        }
+      }
+//      filtered() {
+//        console.log("filtering")
+//        console.log(this.query)
+//        this.displayList = true
+//        if(this.query.length >= this.startAt) {
+//
+//          return this.items.filter(item => {
+//            return item.toLowerCase().indexOf(this.query.toLowerCase()) > -1 && this.query.length < item.length
+//          })
+//
+//        }
+//        console.log("....")
+//      },
     },
+    data () {
+      return {
+        query: '',
+        displayList: false,
+        items: ["abcdef", "abcasdfasdf", "abcmadfmasdfn"],
+      }
+    },
+//    watch: {
+//      selection() {
+//        console.log("selection updated!")
+//        console.log(this.selection)
+//      }
+//    },
     methods: {
-      hello: function() {
-        console.log("hello")
+      reset () {
+        this.query = ''
+      },
+      choose(item) {
+        console.log("item chosen")
+        this.query = item
+        this.$emit('updateselection', item)
       }
     }
   }
 </script>
+
+
+<style>
+  .overlay {
+    position: fixed; /* Sit on top of the page content */
+    width: 10px; /* Full width (cover the whole page) */
+    height: 10px; /* Full height (cover the whole page) */
+    top: 10%;
+    left: 10%;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+    z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+    cursor: pointer; /* Add a pointer on hover */
+  }
+</style>
--- pages/submission/new.vue
+++ pages/submission/new.vue
@@ -7,7 +7,9 @@
       <submit-item
       v-for="rank in teams.length"
       :rank="rank"
-      :keyid="rank">
+      :key="rank"
+      :selections="selections"
+      v-on:updateselection="setSelection">
       </submit-item>
     </div>
   </div>
@@ -21,9 +23,59 @@
     components: {
       SubmitItem
     },
+    methods: {
+      reset() {
+        this.query = ''
+      },
+      select(item) {
+//        console.log("selected!")
+//        console.log(e.currentTarget)
+        this.query = item
+      },
+      setSelection(item) {
+        console.log("setting event selection")
+        this.selections.push(item)
+        console.log(this.selections)
+      }
+    },
+    props: {
+      startAt: {
+        type: Number,
+        default: 3
+      }
+    },
+    computed: {
+      filtered() {
+        if(this.query.length >= this.startAt) {
+
+          return this.items.filter(item => {
+            return item.toLowerCase().indexOf(this.query.toLowerCase()) > -1 && this.query.length < item.length
+
+//            if (item.hasOwnProperty(this.filterKey)) {
+//              return item[this.filterKey].toLowerCase().indexOf(this.query.toLowerCase()) > -1
+//
+//            } else {
+//              console.error("Seems like property ${this.filterKey} doesn't exist on object! ")
+//            }
+          })
+
+        }
+      },
+      isEmpty() {
+        if (typeof this.filtered === 'undefined') {
+          return false
+        } else {
+          return this.filtered.length < 1
+        }
+      }
+    },
     data: function() {
       return {
-        teams: []
+        teams: [],
+//        items: ["abcdef", "abcasdf", "abcwzzfsa"],
+        items: ["abcdef", "abcasdfasdf", "abcmadfmasdfn"],
+        query: '',
+        selections: []
       }
     },
     apollo: {
