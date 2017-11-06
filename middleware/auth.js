52daf8bd2f284bbf4f320bf7c1c56ffdd44aef97
Kyle <none>
2017-11-05 18:17:16 -0800

    Add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,12 @@
+// export default function ({store, error}) {
+export default function (context) {
+  console.log('middleware function...')
+  console.log(context.route)
+  if (context.store.state.userId === 0) {
+    console.log('YOu are not logged in')
+    // context.error({message: 'You are not logged in', statusCode: 403})
+    context.redirect(302, '/auth/login', {message: 'Please login.'})
+  } else {
+    console.log('You are logged in')
+  }
+}
