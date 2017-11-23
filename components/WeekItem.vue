f5ac589ebc71446c108a95f0795e64d7e8788903
Kyle <Kyle.Flavin@gmail.com>
2017-11-23 10:27:44 -0800

    small buttons

--- components/WeekItem.vue
+++ components/WeekItem.vue
@@ -4,10 +4,10 @@
       <b>{{week.date}}</b>
     </td>
     <td>
-      <nuxt-link :to="`week/${week.id}`" class="button is-info is-outlined">Week {{week.id}}</nuxt-link>
+      <nuxt-link :to="`week/${week.id}`" class="button is-small is-info is-outlined">Week {{week.id}}</nuxt-link>
     </td>
     <td>
-      <nuxt-link :to="`week/${week.id}/ranking`" :week="week" class="button is-link is-outlined">Week {{week.id}}</nuxt-link>
+      <nuxt-link :to="`week/${week.id}/ranking`" :week="week" class="button is-small is-link is-outlined">Week {{week.id}}</nuxt-link>
 
     </td>
   </tr>
